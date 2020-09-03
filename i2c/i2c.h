bafe094b3328e4b4eb676abcd758bb6d6316a8ed
Khalil Damouni <KhalilJDamouni@gmail.com>
2020-09-02 22:32:18 -0400

    Added parent and child operations

--- i2c/i2c.h
+++ i2c/i2c.h
@@ -5,4 +5,9 @@
  *      Author: Khalil Damouni
  */
 
-#define 
+
+
+int i2c_init(void);
+int i2c_deinit(void);
+int i2c_parent_operation(enum i2c_operation op, I2C_HandleTypeDef* hi2c, uint16_t DevAddress, uint8_t* pData, uint16_t Size, uint32_t Timeout);
+int i2c_child_operation(enum i2c_operation op, I2C_HandleTypeDef* hi2c, uint16_t DevAddress, uint8_t* pData, uint16_t Size, uint32_t Timeout);
